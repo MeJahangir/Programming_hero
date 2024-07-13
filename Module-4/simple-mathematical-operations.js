2f5cb2964adb608fe75e79f1b1f8b68833d5f60a
MD JAHANGIR ALAM <jahangiralam85860@gmail.com>
2024-07-14 01:02:01 +0600

    variable declear

---
+++ Module-4/simple-mathematical-operations.js
@@ -0,0 +1,17 @@
+// Numeric type add
+var price = 11;
+var age = 24;
+var temperature = 24;
+ var wight = 46;
+ var cash = 2000;
+
+//  String type add
+var person = "sodor uddin";
+var location = 'ullapara';
+var special = 'alia bhatt';
+
+//Boolean type add 
+var serious = true;
+var isFullMarks = true;
+var isSingle = false;
+
