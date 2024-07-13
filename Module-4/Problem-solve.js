c523aafce7aa9c147b858a6cfc0cffef7ba3f3f0
MD JAHANGIR ALAM <jahangiralam85860@gmail.com>
2024-07-13 23:47:03 +0600

    task complete

--- Module-4/Problem-solve.js
+++ Module-4/Problem-solve.js
@@ -1,14 +1,15 @@
-Var Price = 33
+// Var Price = 33
+var Price = 33;
 
-Var Name - Shabana
-
-Var BoxName = ‘Cocola;
-
-Var 88_price = 34;
-
-Var Enum = -1;
-
-Var _$Box’78 = ‘Monika’; 
-
-Var Home-Address = “Kochu Khet”;
-
+// Var Name - Shabana
+var Name = 'shabana';
+// Var BoxName = ‘Cocola;
+var BoxName = 'Cocola';
+// Var 88_price = 34;
+var price88 = 34;
+// Var Enum = -1;
+var EnumValu = -1;
+// Var _$Box’78 = ‘Monika’; 
+var Box78 = "Monika";
+// Var Home-Address = “Kochu Khet”;
+var homeAddress = "kochu khet";
