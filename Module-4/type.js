a3e93c2441bbda4e9147297c3ad888dbe6708aed
MD JAHANGIR ALAM <jahangiralam85860@gmail.com>
2024-07-18 00:13:49 +0600

    submit

---
+++ Module-4/type.js
@@ -0,0 +1,11 @@
+// type check
+var romantic;
+console.log(romantic); // result undifined
+
+var num1 = 0.1;
+var num2 = 0.2;
+
+var sum = num1 + num2;
+sum = sum.toFixed(3);// result er por koto gulo 0 add hobe 
+console.log(sum); // result : result er por 9 ta 0 add hobe;
+
