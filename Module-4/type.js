257c4fea3b288f07024d2eaf0c93d2116db72def
MD JAHANGIR ALAM <jahangiralam85860@gmail.com>
2024-07-18 00:19:13 +0600

    p

--- Module-4/type.js
+++ Module-4/type.js
@@ -7,5 +7,7 @@ var num2 = 0.2;
 
 var sum = num1 + num2;
 sum = sum.toFixed(3);// result er por koto gulo 0 add hobe 
+// tofiexed sob somai string k return kore  er jonno parseFloat use korty hobe
+sum = parseFloat(sum); // akhon number kore print korbe
 console.log(sum); // result : result er por 9 ta 0 add hobe;
 
