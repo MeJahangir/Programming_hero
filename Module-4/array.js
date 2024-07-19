209d0f427c27c5dfc8a558c2fad32bb45364b7fa
MD JAHANGIR ALAM <jahangiralam85860@gmail.com>
2024-07-20 02:18:10 +0600

    submit

--- Module-4/array.js
+++ Module-4/array.js
@@ -1,2 +1,16 @@
 const names = ['jahangir', 'alam','mehedi','nur','juwel','jakariya'];
-console.log(names);
+console.log(names);// array print
+console.log(names[0]); // specific array check
+console.log(typeof[names]);//type check
+names. pop();// last object delet
+names. shift();// fast object delet
+names . unshift('New object add');// fast new object add
+console.log(names); 
+
+var arrayName = ['joy','alam','nur','kasem'];
+var test = arrayName.indexOf('kasem');// array position check
+console.log(test);
+
+var arrayName = ['joy','alam','nur','kasem'];
+arrayName.splice(3);// 3 number object er por theke delet hobe
+console.log(arrayName);
---
+++ Module-4/object.js
@@ -0,0 +1,13 @@
+var jahangir = {
+    fullName : 'Md jahangir alam',
+    age : 25,
+    address : "Dhaka",
+    job : 'Job Holder'
+};
+jahangir.code = 1216 // new value add
+jahangir.welcomeMsg = function () {
+    console.log("Hello There");
+}
+
+jahangir.welcomeMsg();
+console.log(jahangir.welcomeMsg);// object er value check
