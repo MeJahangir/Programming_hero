d0b7bf230937554b139e5adde3b2b88bb0168091
MD JAHANGIR ALAM <jahangiralam85860@gmail.com>
2024-07-20 18:32:01 +0600

    submit

--- Module-4/function.js
+++ Module-4/function.js
@@ -18,9 +18,46 @@ function testFunction(x, y) {
 console.log(testFunction(100 , 100));
 
 function welcomeMsg(names) { // Multy function add 
-    console.log('Welcome Mr' + names);
+    console.log('Welcome M.r' + names);
     return function options(menu) {
-        console.log('Do you like ' + menu + 'Mr' + names);
+        console.log('Do you like ' + menu + " " + 'M.r' + names);
     }
 };
 welcomeMsg(' jahangir alam')('Coffee');
+
+function aFunc(a) {
+    console.log('Before changing the Proparty ' + a.One);
+    a.One = 'Two';
+    console.log('After changing the proparty ' + a.One);
+}
+
+var a = {
+    One: 'One'
+}
+aFunc(a)
+console.log(a);
+
+
+function aSimpleFunction () {
+    console.log('A Simple Function');
+}
+aSimpleFunction();
+
+function sumNumber(x, y) {
+    var sum = x + y;
+    console.log('Sum of ' + x + ' ' + 'and' + ' '  + y + ' '  + "is = " + sum);
+}
+
+sumNumber(10, 10);
+sumNumber(12, 13);
+sumNumber(100, 10);
+sumNumber(101, 11);
+sumNumber(26, 17);
+
+test();
+function test() {
+    var a = 10;
+    var b = 20;
+    var sum = a + b;
+    console.log('Sum: ' + sum);
+}
