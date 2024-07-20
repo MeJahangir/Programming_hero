94ea08504e915ea8a15432023643d98505494468
MD JAHANGIR ALAM <jahangiralam85860@gmail.com>
2024-07-20 12:04:50 +0600

    submit

--- Module-4/function.js
+++ Module-4/function.js
@@ -4,3 +4,23 @@ function myFunction(one, two) {
 }
 text = myFunction(100, 200);
 console.log(text);
+
+function myFunction() {
+    console.log('Hello im from the functon');
+}
+myFunction();
+
+function testFunction(x, y) {
+    let sum = x + y;
+    return sum;
+}
+
+console.log(testFunction(100 , 100));
+
+function welcomeMsg(names) { // Multy function add 
+    console.log('Welcome Mr' + names);
+    return function options(menu) {
+        console.log('Do you like ' + menu + 'Mr' + names);
+    }
+};
+welcomeMsg(' jahangir alam')('Coffee');
