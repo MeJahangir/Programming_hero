45cc4aaaf91c4827dd59c7de8eef44cfec2318f8
MD JAHANGIR ALAM <jahangiralam85860@gmail.com>
2024-07-21 17:40:22 +0600

    submit

--- Module-4/array.js
+++ Module-4/array.js
@@ -1,25 +1,25 @@
-// const names = ['jahangir', 'alam','mehedi','nur','juwel','jakariya'];
-// console.log(names);// array print
-// console.log(names[0]); // specific array check
-// console.log(typeof[names]);//type check
-// names. pop();// last object delet
-// names. shift();// fast object delet
-// names . unshift('New object add');// fast new object add
-// console.log(names); 
+const names = ['jahangir', 'alam','mehedi','nur','juwel','jakariya'];
+console.log(names);// array print
+console.log(names[0]); // specific array check
+console.log(typeof[names]);//type check
+names. pop();// last object delet
+names. shift();// fast object delet
+names . unshift('New object add');// fast new object add
+console.log(names); 
 
-// var arrayName = ['joy','alam','nur','kasem'];
-// var test = arrayName.indexOf('kasem');// array position check
-// console.log(test);
+var arrayName = ['joy','alam','nur','kasem'];
+var test = arrayName.indexOf('kasem');// array position check
+console.log(test);
 
-// var arrayName = ['joy','alam','nur','kasem'];
-// arrayName.splice(3);// 3 number object er por theke delet hobe
-// console.log(arrayName);
+var arrayName = ['joy','alam','nur','kasem'];
+arrayName.splice(3);// 3 number object er por theke delet hobe
+console.log(arrayName);
 
 
-// // var namese = ['jahangir','alam','joy','monir','sumon','habib'];
-// console.log(names.indexOf('nur'));// index check
-// names.splice(1,3)
-// console.log(names);
+// var namese = ['jahangir','alam','joy','monir','sumon','habib'];
+console.log(names.indexOf('nur'));// index check
+names.splice(1,3)
+console.log(names);
 
 
 // array testing code 
--- Module-4/switch.js
+++ Module-4/switch.js
@@ -3,23 +3,38 @@ switch (weekDay) {
     case 'Saturday':
         console.log('Suturday ! Today is closed');      
         break;
-        case 'Sunday':
+    case 'Sunday':
         console.log('Sunday! It is normol day');
     case 'Monday':
         console.log('Monday ! Today is normol day');      
         break;
-        case 'Tuesday':
+    case 'Tuesday':
         console.log('Tuesday! It is normol day');
     case 'Wednesday':
         console.log('Wednesday ! Today is normol day');      
         break;
-        case 'Thurseday':
+    case 'Thurseday':
         console.log('Thurseday! It is normol day');
     case 'Friday':
         console.log('Friday ! Today is closed');      
         break;
-        default:
+default:
         console.log('Not a day');
 }
 
 
+var day = 'friday';
+switch (day) {
+    case 'sunday':
+        console.log('off day');
+        break;
+    case 'saturday':
+        console.log('Off day');
+        break;
+    case 'friday':
+        console.log("Jumma mubarak");
+        break;
+    default:
+        console.log('ajke amar mon valo nai');
+        break;
+};
