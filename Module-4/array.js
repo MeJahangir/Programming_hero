976d0ed271426fa60a804e9e214b681668e5e822
MD JAHANGIR ALAM <jahangiralam85860@gmail.com>
2024-07-21 17:26:14 +0600

    push

--- Module-4/array.js
+++ Module-4/array.js
@@ -1,22 +1,44 @@
-const names = ['jahangir', 'alam','mehedi','nur','juwel','jakariya'];
-console.log(names);// array print
-console.log(names[0]); // specific array check
-console.log(typeof[names]);//type check
-names. pop();// last object delet
-names. shift();// fast object delet
-names . unshift('New object add');// fast new object add
-console.log(names); 
+// const names = ['jahangir', 'alam','mehedi','nur','juwel','jakariya'];
+// console.log(names);// array print
+// console.log(names[0]); // specific array check
+// console.log(typeof[names]);//type check
+// names. pop();// last object delet
+// names. shift();// fast object delet
+// names . unshift('New object add');// fast new object add
+// console.log(names); 
 
-var arrayName = ['joy','alam','nur','kasem'];
-var test = arrayName.indexOf('kasem');// array position check
-console.log(test);
+// var arrayName = ['joy','alam','nur','kasem'];
+// var test = arrayName.indexOf('kasem');// array position check
+// console.log(test);
 
-var arrayName = ['joy','alam','nur','kasem'];
-arrayName.splice(3);// 3 number object er por theke delet hobe
-console.log(arrayName);
+// var arrayName = ['joy','alam','nur','kasem'];
+// arrayName.splice(3);// 3 number object er por theke delet hobe
+// console.log(arrayName);
 
 
-// var namese = ['jahangir','alam','joy','monir','sumon','habib'];
-console.log(names.indexOf('nur'));// index check
-names.splice(1,3)
-console.log(names);
+// // var namese = ['jahangir','alam','joy','monir','sumon','habib'];
+// console.log(names.indexOf('nur'));// index check
+// names.splice(1,3)
+// console.log(names);
+
+
+// array testing code 
+const array = [
+    one = [
+        1,2,6,9
+    ],
+    two = [
+        2,3,4,5,6,7,8
+    ],
+    three = [
+        3,2,1,8
+    ],
+    four = [
+        4,0,3
+    ],
+    five = [
+        5,3,4,0
+    ]
+];
+
+console.log(array[4][3]); //result : 0 print hobe
