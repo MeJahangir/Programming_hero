2c8fb22a612118a31d42acbe3bd2043a04f106f6
MD JAHANGIR ALAM <jahangiralam85860@gmail.com>
2024-07-26 02:56:13 +0600

    push:::

--- Module-4/test.js
+++ Module-4/test.js
@@ -4,10 +4,17 @@ var Person = function (name, age, job) {
     this.job = job;
 };
 
+//--------//
+var info = {
+    name : 'jahangir alam',
+    age : 25, 
+    job : 'privit'
+};
+
 var Teacher = function (name, age, job, subject) {
     Person.call(this, name, age, job);
     this.subject = subject; // Fix: initialize the subject property
 };
-console.log(Person.age);
+console.log(info.name);
 var anyObj = new Teacher('xxxx', 0, 'xxx', 'xmm');
 console.log(anyObj); // Fix: log the created object
